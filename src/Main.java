e187b607469e50c32d96057f5d3b8838e9a07391
joonasalliksaar <joonas.alliksaar@gmail.com>
2024-11-12 10:49:17 +0200

    New code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello universe!");
+        String message = "Welcome to the universe";
+        System.out.println(message);
 
-
+        int number= 10;
+        System.out.println(number);
     }
 }
